2db7d8586bed87c6456daa5f8c28e9958bc3e615
Piotr Kosmowski <kospiotr@gmail.com>
2021-05-19 23:28:04 +0200

    Update

--- components/app.component.js
+++ components/app.component.js
@@ -1,5 +1,50 @@
 Vue.component('app', {
+    data() {
+        return {
+            left: false
+        }
+    },
     template: `
-        <div>test</div>
+<q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
+
+        <q-toolbar-title>
+          Portfolio
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="left" side="left" elevated>
+      <div style="max-width: 350px">
+           <q-list dense padding class="rounded-borders">
+             <q-item clickable v-ripple>
+               <q-item-section>
+                 Projects
+               </q-item-section>
+             </q-item>
+
+             <q-item clickable v-ripple>
+               <q-item-section>
+                 Structure
+               </q-item-section>
+             </q-item>
+
+             <q-item clickable v-ripple>
+               <q-item-section>
+                 Capabilities
+               </q-item-section>
+             </q-item>
+           </q-list>
+         </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
     `
 })
--- portfolio-app.js
+++ portfolio-app.js
@@ -1,25 +1,43 @@
+Promise.retry = function(fn, times, delay) {
+    return new Promise(function(resolve, reject){
+        var error;
+        var attempt = function() {
+            if (times == 0) {
+                reject(error);
+            } else {
+                fn().then(resolve)
+                    .catch(function(e){
+                        times--;
+                        error = e;
+                        setTimeout(function(){attempt()}, delay);
+                    });
+            }
+        };
+        attempt();
+    });
+};
+
 function init(dev){
   baseUrl = dev ? '.' : 'https://kospiotr.github.io/portfolio-app'
 
-  function appendStyle(url){
+  function  appendStyle(url){
       return Promise.resolve(document.getElementsByTagName("head")[0].insertAdjacentHTML(
               "beforeend",
               "<link rel=\"stylesheet\" href=\""+url+"\" />"));
   }
 
   return Promise.all([
-    import('https://cdn.jsdelivr.net/npm/vue'),
-    import('https://cdn.jsdelivr.net/npm/quasar/dist/quasar.umd.min.js'),
-    import(baseUrl+'/portfolio-module.js'),
-    import(baseUrl+'/components/app.component.js'),
     appendStyle('https://cdn.jsdelivr.net/npm/@quasar/extras/material-icons/material-icons.css'),
     appendStyle('https://cdn.jsdelivr.net/npm/quasar/dist/quasar.min.css'),
-  ])
+    import('https://cdn.jsdelivr.net/npm/vue'),
+    import('https://cdn.jsdelivr.net/npm/quasar/dist/quasar.umd.min.js')
+  ]).then(() => Promise.all([
+    import(baseUrl+'/components/app.component.js')
+  ]))
+
 }
 
 init(window.is_dev).then(modules => {
-  var [vue, quasar, portfolio_module] = modules
-
   new Vue({
     el: '#q-app',
     data: function () {
