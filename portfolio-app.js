92cef893ccac05c83bc6e6f6c27aaa864432d13d
Piotr Kosmowski <kospiotr@tlen.pl>
2021-05-19 15:32:42 +0200

    Update portfolio-app.js

--- portfolio-app.js
+++ portfolio-app.js
@@ -1,5 +1,6 @@
 alert('test')
-
+console.log('after')
   import('https://kospiotr.github.io/portfolio-app/portfolio-module.js').then(module => {
+    console.log('in module')
       module.hello('world');
     });
