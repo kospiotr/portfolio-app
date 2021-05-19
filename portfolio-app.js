9e079ad18afbcb88600e666f2467855c2819e8ef
Piotr Kosmowski <kospiotr@tlen.pl>
2021-05-19 16:03:19 +0200

    Update portfolio-app.js

--- portfolio-app.js
+++ portfolio-app.js
@@ -1,10 +1,10 @@
 function importDependencies(dev){
-  baseUrl = dev ? 'https://raw.githack.com/kospiotr/portfolio-app' : 'https://kospiotr.github.io/portfolio-app'
+  baseUrl = dev ? 'https://raw.githack.com/kospiotr/portfolio-app/main' : 'https://kospiotr.github.io/portfolio-app'
   return Promise.all([
     import(baseUrl+'/portfolio-module.js')
   ])
 }
 
-importDependencies(true).then(() => {
+importDependencies(true).then((portfolio_module) => {
   console.log('app loaded')
 })
