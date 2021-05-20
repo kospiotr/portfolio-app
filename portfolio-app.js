a0bfbe838627cc2a85f3b33ccf2fa1366bc1eda0
Piotr Kosmowski <kospiotr@gmail.com>
2021-05-20 10:59:12 +0200

    Update

--- portfolio-app.js
+++ portfolio-app.js
@@ -2,7 +2,8 @@ function normalize(){
     document.querySelectorAll('[style]').forEach(el => el.removeAttribute('style'));
     document.querySelectorAll('link[rel="stylesheet"]').forEach(el => el.parentNode.removeChild(el));
     document.querySelectorAll('style').forEach(el => el.parentNode.removeChild(el));
-    document.body.innerHTML = `<div id="q-app"><app></app></div>`;
+    (document.getElementById('s4-workspace') || document.body).innerHTML = `<div id="q-app"><app></app></div>`;
+
 }
 
 function init(dev){
