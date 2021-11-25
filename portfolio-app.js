90be1d97a56a591f677fa3236a72e9964871ca69
Piotr Kosmowski <kospiotr@gmail.com>
2021-11-25 20:33:17 +0100

    Update portfolio-app.js

--- portfolio-app.js
+++ portfolio-app.js
@@ -2,7 +2,7 @@ function normalize(){
 //    document.querySelectorAll('[style]').forEach(el => el.removeAttribute('style'));
 //    document.querySelectorAll('link[rel="stylesheet"]').forEach(el => el.parentNode.removeChild(el));
 //    document.querySelectorAll('style').forEach(el => el.parentNode.removeChild(el));
-    (document.getElementById('s4-workspace') || document.body).innerHTML = `<div id="q-app"><app></app></div>`;
+//    (document.getElementById('s4-workspace') || document.body).innerHTML = `<div id="q-app"><app></app></div>`;
 
 }
 
